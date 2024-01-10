186dc9d2d8f9f93606c94ab30864d00b07867032
Irfanul383 <irfanulhaque900@gmail.com>
2024-01-10 12:04:45 -0330

    Created components for Contact, Experince and projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => {
+	return (
+		<div name="about" className="h-screen w-full bg-gradient-to-b from-teal-100 to-white">
+			<div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px4 md:flex-row">
+				{' '}
+				Under maintaince
+			</div>
+		</div>
+	);
+};
+
+export default Projects;
