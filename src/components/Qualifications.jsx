4ea532d7a1361df199f0de88fa0f859184c97a29
Irfanul383 <irfanulhaque900@gmail.com>
2024-01-12 07:43:52 -0330

    Added template pojects for projects component

--- src/components/Qualifications.jsx
+++ src/components/Qualifications.jsx
@@ -1,101 +1,117 @@
-import React from 'react';
-import { IoIosSchool, IoMdRibbon } from 'react-icons/io';
-import { FaUnity, FaRegEye, FaCode, FaDatabase, FaPencilRuler } from 'react-icons/fa';
+import React from "react";
+import { IoIosSchool, IoMdRibbon } from "react-icons/io";
+import {
+  FaUnity,
+  FaRegEye,
+  FaCode,
+  FaDatabase,
+  FaPencilRuler,
+} from "react-icons/fa";
 
 const Qualifications = () => {
-	return (
-		<div name="education" className="h-screen w-full bg-gradient-to-b from-teal-100 to-white">
-			<div className="max-w-screen-2xl mx-auto flex flex-col items-center justify-center h-full px-2 md:flex-row">
-				<div className="Education-section w-1/3 shadow-lg">
-					<h2 className="text-3xl sm:text-4xl font-bold">Education</h2>
-					<div className="text-grey-700 py-3">
-						<ul className="Education-list">
-							<li className="flex items-top py-3 text-lg">
-								<div className="flex-shrink-0">
-									<IoIosSchool size={30} className="text-black mr-2" />
-								</div>
-								<div>
-									Bachelor of Science - Computer Science Major & Mathematics Minor at Memorial
-									University of Newfoundland
-								</div>
-							</li>
-							<li className="flex items-top py-3 text-lg">
-								<div className="flex-shrink-0">
-									<IoIosSchool size={30} className="text-black mr-2" />
-								</div>
-								<div>O-levels and A-levels from Pearson Education</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div className="Certification-section ml-10 w-1/3 shadow-lg">
-					<h2 className="flex flex-row text-3xl sm:text-4xl font-bold">Certifications</h2>
-					<div className="text-grey-700 py-4">
-						<ul className="Certification-list">
-							<li className="flex items-top py-3 text-lg">
-								<div className="flex-shrink-0">
-									<IoMdRibbon size={25} className="text-black mr-2" />
-								</div>
-								<div>Amazon Web Services CLF-002 Cloud Practitioner Certificate</div>
-							</li>
-							<li className="flex items-top py-3 text-lg">
-								<div className="flex-shrink-0">
-									<IoMdRibbon size={25} className="text-black mr-2" />
-								</div>
-								<div>
-									Udemy Ultimate AWS Certified Cloud Practitioner CLF-C02 by Stephane Maarek Incentive
-									Program Gold Level
-								</div>
-							</li>
-							<li className="flex items-top py-3 text-lg">
-								<div className="flex-shrink-0">
-									<IoMdRibbon size={25} className="text-black mr-2" />
-								</div>
-								<div>Student Volunteer Bureau Volunteer Incentive Program Gold Level</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div className="skills-section ml-10 w-1/3 shadow-lg">
-					<h2 className="text-3xl sm:text-4xl font-bold">Skills</h2>
-					<div className="text-grey-700 py-4">
-						<ul className="skills-list">
-							<li className="flex items-center py-3 text-lg">
-								<div className="flex-shrink-0">
-									<FaUnity size={25} className="mr-2" />
-								</div>
-								<div>Unity Game Development</div>
-							</li>
-							<li className="flex items-center py-3 text-lg">
-								<div className="flex-shrink-0">
-									<FaRegEye size={25} className="mr-2" />
-								</div>
-								<div>Augmented Reality</div>
-							</li>
-							<li className="flex items-center py-3 text-lg">
-								<div className="flex-shrink-0">
-									<FaCode size={25} className="mr-2" />
-								</div>
-								<div>Web Technologies (React, Bootstrap, Tailwind CSS)</div>
-							</li>
-							<li className="flex items-center py-3 text-lg">
-								<div className="flex-shrink-0">
-									<FaDatabase size={20} className="mr-2" />
-								</div>
-								<div>SQL Queries for Data Optimization</div>
-							</li>
-							<li className="flex items-center py-3 text-lg">
-								<div className="flex-shrink-0">
-									<FaPencilRuler size={20} className="mr-2" />
-								</div>
-								<div>Graphic Design</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      name="education"
+      className="h-screen w-full bg-gradient-to-b from-teal-100 to-white"
+    >
+      <div className="max-w-screen-2xl mx-auto flex flex-col items-center justify-center h-full px-2 md:flex-row">
+        <div className="Education-section w-1/3 shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-105">
+          <h2 className="text-3xl sm:text-4xl font-bold">Education</h2>
+          <div className="text-grey-700 py-3">
+            <ul className="Education-list">
+              <li className="flex items-top py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <IoIosSchool size={30} className="text-black mr-2" />
+                </div>
+                <div>
+                  Bachelor of Science - Computer Science Major & Mathematics
+                  Minor at Memorial University of Newfoundland
+                </div>
+              </li>
+              <li className="flex items-top py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <IoIosSchool size={30} className="text-black mr-2" />
+                </div>
+                <div>O-levels and A-levels from Pearson Education</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="Certification-section ml-10 w-1/3 shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-105">
+          <h2 className="flex flex-row text-3xl sm:text-4xl font-bold">
+            Certifications
+          </h2>
+          <div className="text-grey-700 py-4">
+            <ul className="Certification-list">
+              <li className="flex items-top py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <IoMdRibbon size={25} className="text-black mr-2" />
+                </div>
+                <div>
+                  Amazon Web Services CLF-002 Cloud Practitioner Certificate
+                </div>
+              </li>
+              <li className="flex items-top py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <IoMdRibbon size={25} className="text-black mr-2" />
+                </div>
+                <div>
+                  Udemy Ultimate AWS Certified Cloud Practitioner CLF-C02 by
+                  Stephane Maarek Incentive Program Gold Level
+                </div>
+              </li>
+              <li className="flex items-top py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <IoMdRibbon size={25} className="text-black mr-2" />
+                </div>
+                <div>
+                  Student Volunteer Bureau Volunteer Incentive Program Gold
+                  Level
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="skills-section ml-10 w-1/3 shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-105">
+          <h2 className="text-3xl sm:text-4xl font-bold">Skills</h2>
+          <div className="text-grey-700 py-4">
+            <ul className="skills-list">
+              <li className="flex items-center py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <FaUnity size={25} className="mr-2" />
+                </div>
+                <div>Unity Game Development</div>
+              </li>
+              <li className="flex items-center py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <FaRegEye size={25} className="mr-2" />
+                </div>
+                <div>Augmented Reality</div>
+              </li>
+              <li className="flex items-center py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <FaCode size={25} className="mr-2" />
+                </div>
+                <div>Web Technologies (React, Bootstrap, Tailwind CSS)</div>
+              </li>
+              <li className="flex items-center py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <FaDatabase size={20} className="mr-2" />
+                </div>
+                <div>SQL Queries for Data Optimization</div>
+              </li>
+              <li className="flex items-center py-3 text-lg">
+                <div className="flex-shrink-0">
+                  <FaPencilRuler size={20} className="mr-2" />
+                </div>
+                <div>Graphic Design</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Qualifications;
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,61 +1,55 @@
-import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
-    const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false);
 
-    const links = [
-        { id: 1, link: 'About' },
-        { id: 2, link: 'Qualifications' },
-        { id: 3, link: 'Experience' },
-        { id: 4, link: 'Projects' },
-        { id: 5, link: 'Contact' }
-    ];
+  const links = [
+    { id: 1, link: "About" },
+    { id: 2, link: "Qualifications" },
+    { id: 3, link: "Experience" },
+    { id: 4, link: "Projects" },
+    { id: 5, link: "Contact" },
+  ];
 
-    return (
-        <div className="flex justify-between items-center w-full h-20 px-4 text-black bg-teal-100 fixed">
-            <div>
-                <h1 className="text-3xl ml-2">
-                    Mohamammed Irfanul Haque
-                </h1>
-            </div>
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-black bg-teal-100 fixed">
+      <div>
+        <h1 className="text-3xl ml-2">Mohamammed Irfanul Haque</h1>
+      </div>
 
-            <ul className="hidden md:flex">
-                {links.map(({ id, link }) => (
-                    <li
-                        key={id}
-                        className="px-4 cursor-pointer capitalize font-medium text-gray-700 hover:scale-110 duration-200"
-                    >
-                        {link}
-                    </li>
-                ))}
-            </ul>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-700 hover:scale-110 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
 
-            <div
-                onClick={() => setNav(!nav)}
-                className="cursor-pointer pr-4 z-10 text-gray-700 md:hidden"
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-700 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-teal-100 to-white text-black">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px4 cursor-pointer capitalize py-6 text-4xl"
             >
-                {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-            </div>
-
-            {nav && (
-                <ul
-                    className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-teal-100 to-white text-black"
-                >
-                    {links.map(({ id, link }) => (
-                        <li
-                            key={id}
-                            className='px4 cursor-pointer capitalize py-6 text-4xl'
-                        >
-                            {link}
-
-                        </li>
-                    ))}
-                </ul>
-            )}
-
-        </div>
-    );
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default NavBar;
