c615f789d5a33c0dca04cd666e7d18dc19742b6f
Irfanul383 <irfanulhaque900@gmail.com>
2024-09-09 14:23:56 -0230

    fixed page overlapping bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,46 +12,46 @@ const About = () => {
   return (
     <div
       name="About"
-      className="h-screen w-full bg-gradient-to-b from-teal-100 to-white"
+      className="min-h-screen w-full bg-gradient-to-b from-teal-100 to-white flex flex-col justify-center"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px4 md:flex-row">
-        <div className="flex flex-col px-2 justify-center h-full">
-          <h2 className="text-3xl sm:text-3xl font-bold">
-            Third year student at Memorial University of Newfoundland with a
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center px-4 md:flex-row md:space-x-8">
+        <div className="flex flex-col px-2 py-4 md:py-0 justify-center text-center md:text-left">
+          <h2 className="text-2xl sm:text-3xl font-bold">
+            Third-year student at Memorial University of Newfoundland with a
             Computer Science Major and Mathematics Minor
           </h2>
-          <p className="text-grey-700 py-2 max-w-md">
+          <p className="text-gray-700 py-2 max-w-md">
             👋 Hi there! I'm Mohammed Irfanul Haque, a dedicated and passionate
             software developer with a strong foundation in full-stack
             development, cloud technologies, and DevOps practices. I'm currently
             pursuing a Bachelor of Science in Computer Science with a minor in
             Mathematics at Memorial University of Newfoundland.
           </p>
-          <p className="text-grey-700  max-w-md">
+          <p className="text-gray-700 max-w-md">
             🚀 What I Bring to the Table:
           </p>
-          <p className="text-grey-700 py-1 max-w-md">
+          <p className="text-gray-700 py-1 max-w-md">
             Full-Stack Development: Proficient in front-end technologies like
             Bootstrap and Tailwind CSS, and back-end frameworks including
-            ExpressJS, MongoDB, and NodeJS.{" "}
+            ExpressJS, MongoDB, and NodeJS.
           </p>
-          <p className="text-grey-700 py-1 max-w-md">
+          <p className="text-gray-700 py-1 max-w-md">
             Cloud Expertise: Hands-on experience with AWS services such as EC2,
             ELB, Route 53, and Amplify. I've implemented CI/CD pipelines with
             GitHub Actions and Docker, ensuring streamlined deployments and
             scalable applications.
           </p>
-          <p className="text-grey-700 py-1 max-w-md">
+          <p className="text-gray-700 py-1 max-w-md">
             DevOps Skills: Experienced in setting up and maintaining CI/CD
             pipelines, utilizing Docker for containerization, and employing
             GitHub Actions for continuous integration.
           </p>
-          <p className="text-grey-700 py-1 max-w-md">
+          <p className="text-gray-700 py-1 max-w-md">
             Mobile Development: Developed cross-platform mobile applications
             using React Native and Expo, integrating complex APIs and managing
             data with MongoDB Atlas.
           </p>
-          <div className="flex space-x-4">
+          <div className="flex space-x-4 justify-center md:justify-start">
             {/* LinkedIn */}
             <a
               href="https://www.linkedin.com/in/irfanh383/"
@@ -62,7 +62,6 @@ const About = () => {
                 size={30}
                 className="text-black hover:text-teal-600 cursor-pointer"
               />
-              <span className="text-gray-700 font-bold ml-1"></span>
             </a>
 
             {/* GitHub */}
@@ -75,7 +74,6 @@ const About = () => {
                 size={30}
                 className="text-black hover:text-teal-600 cursor-pointer"
               />
-              <span className="text-gray-700 font-bold ml-1"></span>
             </a>
 
             {/* Email */}
@@ -84,7 +82,6 @@ const About = () => {
                 size={30}
                 className="text-black hover:text-teal-600 cursor-pointer"
               />
-              <span className="text-gray-700 font-bold ml-1"></span>
             </a>
 
             {/* Resume PDF */}
@@ -97,11 +94,10 @@ const About = () => {
                 size={30}
                 className="text-black hover:text-teal-600 cursor-pointer"
               />
-              <span className="text-gray-700 font-bold ml-1"></span>
             </a>
           </div>
-          <div>
-            <button className="group w-fit px-6 py-3 my-2 flex items-center rounded-md bg-black text-white cursor-pointer">
+          <div className="mt-4">
+            <button className="group w-fit px-6 py-3 flex items-center rounded-md bg-black text-white cursor-pointer">
               Next
               <span className="group-hover:rotate-90 duration-200">
                 <IoIosArrowForward size={15} className="ml-1" />
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,16 +4,16 @@ const Experience = () => {
   return (
     <div
       name="Experience"
-      className="w-full bg-gradient-to-b from-teal-100 to-white md:h-screen mt-5 mb-5 md:mt-0 md:mb-0"
+      className="w-full bg-gradient-to-b from-teal-100 to-white py-10 md:h-screen  mt-5 mb-5 md:mt-0 md:mb-0"
     >
-      <div className="max-w-screen-2xl mx-auto flex flex-col items-center justify-center h-full px-2 md:flex-row md:space-y-0 space-y-10">
+      <div className="max-w-screen-2xl mx-auto flex flex-col items-center justify-center px-2 space-y-10 md:space-y-0 md:space-x-8 md:flex-row">
         {/* Experience Section 1 */}
-        <div className="Experience-section bg-gradient-to-b from-white to-teal-100 mx-10 shadow-md shadow-gray-600 rounded-lg duration-200  md:w-1/3">
-          <h2 className="text-2xl m-4 sm:text-3xl font-bold">
-            Full-Stack developer | Muslim Student Association, Memorial
+        <div className="Experience-section bg-gradient-to-b from-white to-teal-100 shadow-md shadow-gray-600 rounded-lg duration-200 w-full md:w-1/3 mx-5 p-4">
+          <h2 className="text-2xl sm:text-3xl font-bold mb-4">
+            Full-Stack Developer | Muslim Student Association, Memorial
             University of Newfoundland (Dec 2023 - Current)
           </h2>
-          <div className="m-4 text-grey-700 py-3">
+          <div className="text-gray-700 mb-4">
             <p>
               As a Full-Stack Developer for the Muslim Student Association at
               Memorial University of Newfoundland, I developed and maintained a
@@ -26,22 +26,22 @@ const Experience = () => {
               documentation to facilitate onboarding for new developers.
             </p>
           </div>
-          <div className="skills-learned">
-            <h3 className="m-4 text-1xl font-bold">Skills Learned:</h3>
-            <ul className="m-4">
-              <li>* Web Design using Bootstrap</li>
-              <li>* UI Development with React</li>
-              <li>* Web Maintenance</li>
+          <div>
+            <h3 className="text-xl font-bold mb-2">Skills Learned:</h3>
+            <ul className="list-disc pl-6">
+              <li>Web Design using Bootstrap</li>
+              <li>UI Development with React</li>
+              <li>Web Maintenance</li>
             </ul>
           </div>
         </div>
         {/* Experience Section 2 */}
-        <div className="Experience-section bg-gradient-to-b from-white to-teal-100 mx-10 shadow-md shadow-gray-600 rounded-lg duration-200 md:w-1/3">
-          <h2 className="text-2xl m-4 sm:text-3xl font-bold">
+        <div className="Experience-section bg-gradient-to-b from-white to-teal-100 shadow-md shadow-gray-600 rounded-lg duration-200 w-full md:w-1/3 mx-5 p-4">
+          <h2 className="text-2xl sm:text-3xl font-bold mb-4">
             Admissions Department Student Assistant | Memorial University of
             Newfoundland (Oct 2021-Current)
           </h2>
-          <div className="m-4 text-grey-700 py-3">
+          <div className="text-gray-700 mb-4">
             <p>
               As a Student Assistant at the Registrar’s Office at Memorial
               University of Newfoundland, I automated data entry forms for
@@ -51,22 +51,22 @@ const Experience = () => {
               efficient forms with VBA to enhance data management processes.
             </p>
           </div>
-          <div className="skills-learned">
-            <h3 className="m-4 text-1xl font-bold">Skills Learned:</h3>
-            <ul className="m-4">
-              <li>* Excel Sheet Management and Optimization</li>
-              <li>* SQL Query Optimization</li>
-              <li>* Data Management</li>
+          <div>
+            <h3 className="text-xl font-bold mb-2">Skills Learned:</h3>
+            <ul className="list-disc pl-6">
+              <li>Excel Sheet Management and Optimization</li>
+              <li>SQL Query Optimization</li>
+              <li>Data Management</li>
             </ul>
           </div>
         </div>
         {/* Experience Section 3 */}
-        <div className="Experience-section bg-gradient-to-b from-white to-teal-100 mx-10 shadow-md shadow-gray-600 rounded-lg duration-200 md:w-1/3">
-          <h2 className="text-2xl m-4 sm:text-3xl font-bold">
+        <div className="Experience-section bg-gradient-to-b from-white to-teal-100 shadow-md shadow-gray-600 rounded-lg duration-200 w-full md:w-1/3 mx-5 p-4">
+          <h2 className="text-2xl sm:text-3xl font-bold mb-4">
             Graphic Coordinator | Student Volunteer Bureau, Memorial University
             of Newfoundland (Sep 2022-Apr 2023)
           </h2>
-          <div className="m-4 text-grey-700 py-3">
+          <div className="text-gray-700 mb-4">
             <p>
               Serving as the Graphic Coordinator at the Student Volunteer
               Bureau, Memorial University of Newfoundland, I led a graphic
@@ -74,12 +74,12 @@ const Experience = () => {
               alignment, and supported students in volunteering activities.
             </p>
           </div>
-          <div className="skills-learned">
-            <h3 className="m-4 text-1xl font-bold">Skills Learned:</h3>
-            <ul className="m-4">
-              <li>* Graphic Design</li>
-              <li>* Coordination and Communication</li>
-              <li>* Volunteer and Community Engagement</li>
+          <div>
+            <h3 className="text-xl font-bold mb-2">Skills Learned:</h3>
+            <ul className="list-disc pl-6">
+              <li>Graphic Design</li>
+              <li>Coordination and Communication</li>
+              <li>Volunteer and Community Engagement</li>
             </ul>
           </div>
         </div>
